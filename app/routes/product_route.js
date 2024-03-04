31f489cb43597e7ff9320d245a00a696a3ce9453
adhlira <adhliramadhan68@gmail.com>
2024-03-04 21:38:25 +0700

    add route put on product route

--- app/routes/product_route.js
+++ app/routes/product_route.js
@@ -21,4 +21,18 @@ router.get("/products/:id", async (req, res) => {
   }
 });
 
+router.put("/products/:id", async (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.status(400).json({ message: "Invalid ID" });
+  } else {
+    const product = await prisma.product.findFirst({ where: { id: +req.params.id } });
+    if (!product) {
+      res.status(404).json({ message: "Product Not Found" });
+    } else {
+      const product_updated = await prisma.product.update({ where: { id: +req.params.id }, data: req.body });
+      res.status(200).json({ message: "Product updated", product_updated });
+    }
+  }
+});
+
 export default router;

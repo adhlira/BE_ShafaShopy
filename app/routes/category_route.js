45ffef46e125f0aa5318751e60cbd1f112f30084
adhlira <adhliramadhan68@gmail.com>
2024-06-18 14:31:17 +0700

    post data

--- app/routes/category_route.js
+++ app/routes/category_route.js
@@ -8,4 +8,17 @@ router.get("/categories", async (req, res) => {
   res.status(200).json(categories);
 });
 
+router.post("/categories", async (req, res) => {
+  const { name } = req.body;
+  const duplikatCategory = await prisma.category.findFirst({ where: { name: req.body.name } });
+  if (!req.body.name) {
+    res.status(400).json({ message: "Data tidak lengkap" });
+  } else if (duplikatCategory) {
+    res.status(400).json({ message: "Nama category yang dimasukkan sudah ada" });
+  } else {
+    const category = await prisma.category.create({ data: { name } });
+    res.status(200).json({ message: "Berhasil menambahkan data category", category });
+  }
+});
+
 export default router;

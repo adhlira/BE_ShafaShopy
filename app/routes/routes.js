f4bf4d46c3ee034752aaf40f49c615f74ac4cf5f
adhlira <adhliramadhan68@gmail.com>
2024-06-23 15:19:48 +0700

    buat route selling price

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import productroute from "./product_route.js";
 import categoryroute from "./category_route.js";
 import colorroute from "./color_route.js";
+import sellingpriceroute from "./sellingprice_route.js";
 
 const router = Router();
 
 router.use(productroute);
 router.use(categoryroute);
 router.use(colorroute);
+router.use(sellingpriceroute);
 
 export default router;
---
+++ app/routes/sellingprice_route.js
@@ -0,0 +1,71 @@
+import Router from "express";
+import prisma from "../helper/prisma.js";
+
+const router = Router();
+
+router.get("/sellingprice", async (req, res) => {
+  const results = await prisma.sellingPrice.findMany({
+    include: {
+      Product: { include: { Color: { select: { name: true } } } },
+    },
+  });
+  res.status(200).json(results);
+});
+
+router.get("/products/:id", async (req, res) => {
+  const productId = parseInt(req.params.id);
+  if (isNaN(productId)) {
+    res.status(400).json({ message: "Invalid ID" });
+  } else {
+    try {
+      const product = await prisma.product.findFirst({
+        where: { id: productId },
+        include: {
+          Category: { select: { name: true } },
+          Color: { select: { name: true } },
+          SellingPrice: { select: { price0: true, price1: true, price2: true, price3: true, price4: true, price5: true } },
+        },
+      });
+
+      if (!product) {
+        res.status(404).json({ message: "Product Not Found" });
+      } else {
+        res.status(200).json(product);
+      }
+    } catch (error) {
+      console.error("Error fetching product:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+});
+
+router.post("/products", async (req, res) => {
+  const { category_id, color_id, name, purchase_price, stock, description } = req.body;
+  if (!req.body.name) {
+    res.status(400).json({ message: "Data tidak lengkap" });
+  } else {
+    const product = await prisma.product.create({ data: { category_id, color_id, name, purchase_price, stock, description } });
+    res.status(200).json({ message: "Berhasil menambahkan data product", product });
+  }
+});
+
+router.put("/products/:id", async (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.status(400).json({ message: "Invalid ID" });
+  } else {
+    const product = await prisma.product.findFirst({ where: { id: +req.params.id } });
+    if (!product) {
+      res.status(404).json({ message: "Product Not Found" });
+    } else {
+      const product_updated = await prisma.product.update({ where: { id: +req.params.id }, data: req.body });
+      res.status(200).json({ message: "Product updated", product_updated });
+    }
+  }
+});
+
+router.delete("/products/:id", async (req, res) => {
+  await prisma.product.delete({ where: { id: +req.params.id } });
+  res.status(200).json({ message: "Product deleted" });
+});
+
+export default router;

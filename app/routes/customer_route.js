e8730e7996876646577c1b35db9f7ba92d415b4e
adhlira <adhliramadhan68@gmail.com>
2024-06-26 17:02:44 +0700

    fix customer route

--- app/routes/customer_route.js
+++ app/routes/customer_route.js
@@ -4,7 +4,7 @@ import prisma from "../helper/prisma.js";
 const router = Router();
 
 router.get("/customers", async (req, res) => {
-  const results = await prisma.customers.findMany({ include: { Level: { select: { level: true } } } });
+  const results = await prisma.customers.findMany({ include: { Level: { select: { level: true } } }, orderBy: { level_id: "asc" } });
   res.status(200).json(results);
 });
 
